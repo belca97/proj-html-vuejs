292f982765aeb9907866fe76bf671019841534fa
Matteo Belcari <matteobelcari977@gmail.com>
2022-03-03 23:02:24 +0100

    Fine gran parte main statistico

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,7 +1,119 @@
 <template>
-  <div>
+  <main>
+    <div class="container ms_container">
+      <!-- inizio prima parte main in maniera statica -->
+          <div class="text-first-part h-100 py-3">
+            <span>OUR PRODUCT</span>
+            <p>All our delectable pastries are backed fresh in our Kitchen very morning, and are made with all-natural, all organic ingredients</p>
+            <button>Star shopping</button>
 
+          </div>
+          <div class="img-center-first-part h-100">
+            <img src="../assets/hover-card1.png" alt="">
+          </div>
+          <div class="img-right-first-part h-100">
+              <img src="../assets/hover-card1.png" alt="">
+          </div>
+
+    </div>
+    <!-- fine prima parte main statico -->
+      <!-- inizio seconda parte main in maniera statica -->
+  <div class="container ms_container ">
+        <div class="text-center text-second-part">
+            <span>CORPORATE E WEDDINGS</span>
+            <p>Baking Special Moments</p>
+        </div>
+        <div class="img-second-part">
+          <img class="w-100" src="../assets/hover-img1.png" alt="">
+        </div>
   </div>
+       <!-- fine seconda parte main statico -->
+       <!-- inizio terza parte main statico -->
+      <div class="container ms_padding-80">
+          <div class="ms_container-third">
+              <div class="text-third-part">
+                <p>Find a freshly baked product perfect for you</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore ut inventore et porro at consequatur praesentium corporis veritatis delectus culpa quo nesciunt, fuga maiores libero voluptates nulla rerum. Accusamus, ut?</p>
+                <button>Shop All Product</button>
+              </div>
+
+              <div class="img-third-part">
+                <img class="w-100" src="../assets/hover-card2.png" alt="">
+                
+              </div>
+
+              <div class="img-third-part">
+                <img class="w-100" src="../assets/hover-card2.png" alt="">        
+              </div>
+              
+              <div class="img-third-part">
+                <img class="w-100" src="../assets/hover-card2.png" alt="">        
+              </div>
+              
+              <div class="img-third-part">
+                <img class="w-100" src="../assets/hover-card2.png" alt="">        
+              </div>
+          </div>
+      </div>
+      <!-- fine terza parte main statico -->
+      <!-- inizio quarta parte main statico -->
+      <div class="ms_container-80 background-img-bread mb-3 mt-5 p-5">
+        <div class="top-text-01">
+          <div class="text-left">
+            <p>Tell us what you have in ming</p>
+          </div>
+          <div class="text-right">
+            <span>01</span>
+          </div>
+        </div>
+        <div class="text-bottom-right">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur quam eius qui iure corporis placeat nesciunt dicta minima, ducimus et quisquam recusandae doloribus atque praesentium at ipsam inventore iste optio.</p>
+        </div>
+        <!-- fine quarta parte main statico -->
+
+          </div>
+            <!-- inizio quinta parte main statico -->
+          <div class="ms_container-80 ms_container mb-3">
+            <div class="img-left-quinta-parte background-img-bread h-100 p-5">
+                <div class="top-text-01">
+                  <div class="text-left">
+                    <p>Tell us what you have in ming</p>
+                  </div>
+                  <div class="text-right">
+                    <span>02</span>
+                  </div>
+               </div>
+                <div class="text-bottom-left w-50">
+                  <span>wewe</span>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui dolorem nam quas autem nesciunt molestias cumque itaque illum corrupti odio aliquid quam atque, unde veritatis laborum dolore suscipit recusandae porro?</p>
+                </div>
+            </div>
+            <div class="testo-right-quinta-parte h-100 p-5">
+              <div class="h-100 d-flex  flex-column justify-content-center">
+                <span>wewe</span>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia eum non eveniet cum debitis delectus consequuntur amet! Quisquam numquam eius odio beatae facilis accusantium possimus, libero corporis voluptatum, repellat porro?</p>
+                <span>wewe</span>
+              </div>
+            </div>
+          </div>
+              <!--fine quinta parte main statico -->
+              <!-- inizio sesta parte main statico -->
+          <div class="ms_container-80 background-img-bread-2 mb-3 p-5 text-white">
+               <div class="top-text-01">
+                  <div class="text-left">
+                    <p>Tell us what you have in ming</p>
+                  </div>
+                  <div class="text-right">
+                    <span>02</span>
+                  </div>
+               </div>
+                <div class="text-bottom-left w-50">
+                  <span>wewe</span>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui dolorem nam quas autem nesciunt molestias cumque itaque illum corrupti odio aliquid quam atque, unde veritatis laborum dolore suscipit recusandae porro?</p>
+                </div>
+          </div>
+          <!-- fine sesta parte main statico -->
+  </main>
 </template>
 
 <script>
@@ -10,6 +122,183 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang='scss'>
 
+  @import '../assets/variabili.scss';
+  main{
+    margin-top: 100px;
+    /*inizio prima parte del main in maniera statica */
+    .text-first-part{
+        float: left;
+        width: calc(100% / 3);
+        button{
+             padding: 10px 32px;
+             background-color: $colorTitolo;
+             border: 1px solid white;
+             border-radius: 8px;
+             font-size: 20px;
+             color: white;          
+        }
+        p{
+          color: $colorTitolo;
+          font-size: 30px;
+          margin: 40px 0px;
+        }
+        span{
+          color: $colorTitolo;
+          font-size: 10px;
+        }
+    }
+    .img-center-first-part{
+        float: left;
+        width: calc(100% / 3 );
+        height: 100%;
+        img{
+          width: 100%;
+        }
+    }
+    .img-right-first-part{
+        float: right;
+        width: calc(100% / 3);
+        height: 100%;
+        img{
+          width: 100%;
+        }
+    }
+  }
+      /*fine prima parte del main in maniera statica */
+
+
+    /*inizio seconda parte del main in maniera statica */
+
+  .text-second-part{
+    height: 100px;
+    span{
+       color: $colorTitolo;
+       font-size: 10px;
+    }
+    p{
+       color: $colorTitolo;
+       font-size: 30px;
+       margin: 20px 0px;
+    }
+  }
+
+
+   /*fine seconda parte del main in maniera statica */
+
+
+    /*inizio terza parte del main in maniera statica */
+    .ms_padding-80{
+      padding: 80px 0px;
+    }
+    .ms_container-third{
+    height: 350px!important;
+    .text-third-part{
+      width: calc(100% / 5);
+      float: left;
+        button{
+             padding: 10px 32px;
+             background-color: $colorTitolo;
+             border: 1px solid white;
+             border-radius: 8px;
+             font-size: 20px;
+             color: white;   
+        }
+    }
+    .img-third-part{
+      width: calc(100% / 5);
+      float: left;
+      img{
+        object-fit: contain;
+      }      
+    }
+  }
+
+
+    /*fine terza parte del main in maniera statica */
+    /* inizio quarta parte main statica */
+        .top-text-01::after{
+      clear: both;
+      content: '';
+      display: table;
+    }
+    .top-text-01{
+      height: 100px;
+        .text-left{
+          float: left;
+          width: 50%;
+          text-align: left;
+          padding-left: 10px;
+        }
+        .text-right{
+          float: right;
+          width: 10%;
+          text-align: left;
+          padding-right: 10px;
+        }
+    }
+    .text-bottom-right{
+      width: 50%;
+      float: right;
+      display: flex;
+      align-items: end;
+      height: 80%;
+    }
+
+    /* fine quarta parte main statica */
+    /* inizio quinta parte main statica */
+    .img-left-quinta-parte{
+      float: left;
+      width: 49%;
+      height: 90%;
+    }
+    .testo-right-quinta-parte{
+      float: right;
+      width: 49%;
+      height: 90%;
+    }
+    .text-bottom-left{
+      height: 80%;
+      display: flex;
+      justify-content: flex-end;
+      flex-direction: column;
+    }
+
+    .testo-right-quinta-parte{
+      background-color: rgb(238, 231, 231);
+      p{
+        color: $colorTitolo;
+        font-size: 25px;
+      }
+    }
+
+     /* fine quinta parte main statica */
+
+   /* inizio generale del main */
+
+  .ms_container::after{
+    clear: both;
+    content: '';
+    display: table;
+  }
+  .ms_container-third::after{
+    clear: both;
+    content: '';
+    display: table;
+  }
+  .ms_container-80{
+    width: 80%;
+    height: 500px;
+    margin: 0 auto;
+  }
+  .background-img-bread{
+    background-image: url('../assets/pexels-karolina-grabowska-4495761.jpg');
+    background-size: cover;
+  }
+  .background-img-bread-2{
+    background-image: url('../assets/pexels-pixabay-209196.jpg');
+    background-size: cover;
+    background-position-y: -7em;
+  }
 </style>
